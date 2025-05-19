fd31051805f35e4aea70f622f125cb5715d73db7
bernat alcudia <bernatalcudia@gmail.com>
2025-05-19 10:41:07 +0200

    test:cheerio basics

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const main = () => {
-    console.log('hi')
-}
+import * as cheerio from 'cheerio'
 
+
+
+const main = () => {
+    const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+
+    const text = $('h2.title').text()
+
+    console.log(text)
+
+}
 main()
