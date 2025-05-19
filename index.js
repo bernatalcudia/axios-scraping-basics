d5ca4df3c22b7be23b9cca6cd0eba8e528a57266
bernat alcudia <bernatalcudia@gmail.com>
2025-05-19 10:50:54 +0200

    test(api):axios basics

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import * as cheerio from 'cheerio'
+import axios from 'axios'
 
 
 
-const main = () => {
-    const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+const main = async () => {
+    const cheerioDocsWeb = await axios.get('https://:cheerio.js.org/docs/intro')
+    // const $ = cheerio.load('<h2 class="title">Hello world</h2>')
 
-    const text = $('h2.title').text()
+    // const text = $('h2.title').text()
 
-    console.log(text)
+    // console.log(text)
+    console.log(cheerioDocsWeb)
 
 }
 main()
